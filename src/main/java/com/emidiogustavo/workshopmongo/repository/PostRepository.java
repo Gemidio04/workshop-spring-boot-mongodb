bdc082de524e0a1eff9a27a94d36824edf3f313d
Gustavo Emídio <gemidio2004@gmail.com>
2024-06-21 10:15:51 -0300

    Consulta simples com @query

--- src/main/java/com/emidiogustavo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/emidiogustavo/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.emidiogustavo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.emidiogustavo.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text); 
 }
